79fa298f84176dda564af298f2a159cfb49d0751
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-09-08 23:04:24 +0000

    size print

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * main - start point
+ *
+ * printf: prints strings
+ *
+ * Return: zero
+ */
+
+int main(void)
+{
+	printf("Size of char: 1 byte(s)");
+	printf("Size of an int: 4(s)");
+	printf("Size of a long int: 8 byte(s)");
+	printf("Size of a long long int: 8 byte(s)");
+	printf("Size of a float: 4 byte(s)");
+	return (0);
+}
