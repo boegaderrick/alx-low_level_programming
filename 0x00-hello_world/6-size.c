61fba7cccbdd16e5baa6ec9d6f06364b2a4f5b76
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-09-08 23:10:40 +0000

    size print

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,8 +10,8 @@
 
 int main(void)
 {
-	printf("Size of char: 1 byte(s)\n");
-	printf("Size of an int: 4(s)\n");
+	printf("Size of a char: 1 byte(s)\n");
+	printf("Size of an int: 4 byte(s)\n");
 	printf("Size of a long int: 8 byte(s)\n");
 	printf("Size of a long long int: 8 byte(s)\n");
 	printf("Size of a float: 4 byte(s)\n");
