55ae62f94cf25f0ff7171dd5f07702f8bac6e46b
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-09-08 22:42:57 +0000

    printf code

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * main - start point
+ *
+ * printf: prints a string
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
