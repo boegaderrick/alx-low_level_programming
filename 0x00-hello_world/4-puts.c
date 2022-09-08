072ca0b6cef918a18d239581cc250cd658b26437
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-09-08 22:35:37 +0000

    my first code in c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,11 @@
 /**
  * main - start point
  *
- * description: program prints a string
+ * Description: program prints a string
  *
- * return: zero
+ * Return: zero
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
