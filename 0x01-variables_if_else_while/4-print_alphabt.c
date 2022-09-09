ee9b331b06bfceb786b2d3d44b7fbecd7d0195f8
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-09-09 22:31:12 +0000

    alphabet -2

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,30 @@
+#include<stdio>
+/**
+ * main - entry point
+ *
+ * Description: Prints alphabets except q and e
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char alphabet;
+
+	alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		if (alphabet == 'e', 'q')
+		{
+			alphabet ++2;
+		}
+		else
+		{
+			putchar(alphabet);
+			alphabet++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
