63ffded383bea0bc73b7de6fc83cf786df0f13f1
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-09-09 22:03:33 +0000

    alphaBETS

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+/**
+ * main - program entry point
+ *
+ * Description: Program prints alphabet in lower and upper cases
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char alphabet, ALPHABET;
+
+	alphabet = 'a'
+	ALPHABET = 'A'
+	while (alphabet <= z)&&(ALPHABET <= Z)
+	{
+		putchar(alphabet);
+		putchar(ALPHABET);
+		alphabet++
+		ALPHABET++
+	}
+
+	putchar('\n');
+	return (0);
+}
