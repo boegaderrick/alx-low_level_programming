58d7d9c7c85f0019f3f040deb5a442586038e958
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-09-09 23:37:39 +0000

    numbers and commas

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: prints numbers separated by commas
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int num;
+
+	num = '0';
+
+	while (num <= 9)
+	{
+		for num
+		{
+			putchar(num);
+			printf(",");
+			num++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
