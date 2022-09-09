6cde90a6d7c382ceafc46b982e88c545b5a63c0d
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-09-09 21:02:17 +0000

    last_digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -26,9 +26,9 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, last_digit);
 	}
-	else if (last_digit < 6 !0)
+	else if (last_digit < 6)&&(last_digit !0)
 	{
-		puts("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
 	}
 
 	return (0);
