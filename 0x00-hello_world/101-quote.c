cf33d3a48449e3f9a352f2d6084cab131f0e9806
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-09-09 00:45:14 +0000

    big piece of error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,12 @@
-#include<stdio.h>
+#include <unistd.h>
 
 /**
- * main - entry point
- *
- * Return: 1
+ *main - prints the standrad error sandwiched around the  standard outpu/
+ t
+ *Return:returns 1
  */
-
 int main(void)
 {
-	("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
-	printf("\n");
-	return (1);
+		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+			return (1);
 }
