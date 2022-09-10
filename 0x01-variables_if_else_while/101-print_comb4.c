7a8355d5a16d9376049e828dd7a43722caccf79b
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-09-10 02:53:51 +0000

    3 unique

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,7 +10,7 @@
 int main(void)
 {
 	int num1, num2, num3;
-	
+
 	for (num1 = 0; num1 < 8; num1++)
 	{
 		for (num2 = num1 + 1; num2 < 9; num2++)
@@ -20,7 +20,7 @@ int main(void)
 				putchar((num1 % 10) + '0');
 				putchar((num2 % 10) + '0');
 				putchar((num3 % 10) + '0');
-				
+
 				if (num1 == 7 && num2 == 8 && num3 == 9)
 					continue;
 				
@@ -29,8 +29,8 @@ int main(void)
 			}
 		}
 	}
-	
+
 	putchar('\n');
-	
+
 	return (0);
 }
