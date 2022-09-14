cd6ea0bd995844a310088bca0578dd1242a4e10b
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-09-14 07:13:43 +0000

    print alpha func

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,23 +1,6 @@
 #include<stdio.h>
 
 /**
-* main - entry point
-* 
-* Description: calls print_alphabet
-*
-* Return: 0
-*/
-
-void print_alphabet(void);
-
-int main(void)
-{
-	void print_alphabet(void);
-	print_alphabet();
-	return (0);
-}
-
-/**
  * print_alphabet - alphabet function
  *
  * description: prints alphabet
