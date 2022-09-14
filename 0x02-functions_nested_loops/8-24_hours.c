cec8ce0b611ff57ad9784965dd9b6d003482815f
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-09-14 18:01:54 +0000

    jack bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * jack_bauer - entry point
+ *
+ * Description: prints all minutes in a day
+ */
+
+void jack_bauer(void)
+{
+	int minute;
+
+	minute = 00 : 00;
+
+	while (minute <= 23 : 59)
+	{
+		_putchar(minute);
+		_putchar('\n');
+		minute++;
+	}
+}
