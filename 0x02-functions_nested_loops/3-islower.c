45b1c03961d387b339ca153c628c2bb93150319c
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-09-14 10:20:27 +0000

    check case func

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _islower - entry point
+ *
+ * Description: checks if char is lowercase
+ *
+ *Return: 1 or 0
+ */
+
+int _islower(int c);
+{
+	if islower()
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
