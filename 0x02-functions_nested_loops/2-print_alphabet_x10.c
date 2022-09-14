348840194d1514709157f146e0cc9c764b672fda
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-09-14 09:32:23 +0000

    10x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - entry point
+ *
+ * Description: prints alphabet x10
+ *
+ * Return: 
+ */
+
+void print_alphabet_x10(void)
+{
+	char alphabet = 'a';
+	int line_count = 0;
+
+	while (line_count <= 10);
+	{
+		while (alphabet <= 'z');
+		{
+			_putchar(alphabet);
+			alphabet++;
+		}
+		line_count++;
+	}
+	_putchar('\n');
+}
