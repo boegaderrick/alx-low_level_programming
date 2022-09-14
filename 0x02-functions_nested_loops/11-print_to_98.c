9429dd8285d46e69bd969fc6528b6a034b9dde42
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-09-14 22:51:07 +0000

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,7 +14,7 @@ void print_to_98(int n)
 		{
 			if (n == 98)
 			{
-				printf("%d", n);
+				printf("%d ", n);
 				n++;
 			}
 			else
@@ -30,12 +30,12 @@ void print_to_98(int n)
 		{
 			if (n == 98)
 			{
-				printf("%d", n);
+				printf("%d ", n);
 				n--;
 			}
 			else
 			{
-				printf("%d", n);
+				printf("%d, ", n);
 				n--;
 			}
 		}
