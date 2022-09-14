3ee334c171db96573e24633be85d7cc1e8d0238d
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-09-14 21:23:50 +0000

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *
+ * times_table - function prints times table
  */
 
 void times_table(void)
@@ -25,7 +25,7 @@ void times_table(void)
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
-				_putcahr(prod);
+				_putchar(prod);
 			}
 			else if (prod >= 10)
 			{
