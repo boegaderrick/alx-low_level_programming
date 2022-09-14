eb9b3e17a73a2a5c10efe1d4de56d5cb40c43dd8
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-09-14 12:18:36 +0000

    check number sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_sign - entry point
+ *
+ * Description: checks sign on number
+ *
+ * @n: number to be checked
+ *
+ * Return: -1, 0 or 1
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
