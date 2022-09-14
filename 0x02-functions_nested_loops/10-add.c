73cc5c30cb402adcc8b96e9b28ff7e9208589df7
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-09-14 22:18:29 +0000

    sum func

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,11 +3,16 @@
 /**
  * add - adds two numbers
  *
+ * @a: input number to be added
+ *
+ * @b: "
+ *
  * Return: result of the operation
  */
 
 int add(int a, int b)
 {
 	int result = a + b;
+
 	return (result);
 }
