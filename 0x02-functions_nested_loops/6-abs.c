fcc848ba242160218eebdff760ab7667b2321981
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-09-14 15:20:42 +0000

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _abs - entry point
+ *
+ * Description: checks absolute value
+ *
+ * Return: 0
+ */
+
+int _abs(int)
+{
+	int i;
+
+	if (i >= 0)
+	{
+		_putchar(i);
+	}
+	else
+	{
+		_putchar(-i);
+	}
+	return (0);
+}
