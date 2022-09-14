7877ee1127f4e1df16d525f1ef9c2e8ae0c51234
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-09-14 12:03:22 +0000

    check if c is letter

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _isalpha - entry point
+ *
+ * Description: check if character is letter
+ *
+ * @c: character to be checked
+ *
+ * Return: 1 or 0
+ */
+
+int _isalpha(int c)
+{
+	if ((c <= 'z' && c >= a) || (c <= 'Z' && c >= 'A'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
