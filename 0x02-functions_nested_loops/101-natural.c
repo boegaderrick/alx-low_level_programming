da6c0b4aea4ab56e1da98a2f2f3c5b8b2a60e6d7
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-09-14 23:17:56 +0000

    sum of multiples

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/***/
+
+int main(void)
+{
+	int n = 1;
+	int sum;
+
+	while (n < 1024)
+	{
+		if ((n % 3 = 0) || (n / 5 = 0))
+		{
+			sum = sum + n;
+		}
+		else
+		{
+			continue;
+		}
+		n++
+	}
+	return (sum);
+}
