0586527bf6935264e77a95650a7a68e47413a0d3
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-09-15 00:12:54 +0000

    custom times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * times_table - function prints times table
+ * print_times_table - function prints times table
  */
 
-void times_table(int n)
+void print_times_table(int n)
 {
 	int row, col, prod;
 
