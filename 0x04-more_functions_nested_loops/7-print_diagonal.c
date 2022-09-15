5f8943ecdd62b056c51e8dd6de3006bee87d0d29
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-09-15 20:58:24 +0000

    diagonal print

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_line - prints line
+ *
+ * @n: range to which to print
+ */
+
+void print_line(int n)
+{
+	int i, spc;
+
+	if (n > 0)
+	{
+		for (i = 1; i <= n; i++)
+		{
+			for (spc = 1; spc <= n; spc++)
+			{
+				if (spc == i)
+				{
+					_putchar('\\');
+				}
+				else if (spc < i)
+				{
+					_putchar(' ');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
