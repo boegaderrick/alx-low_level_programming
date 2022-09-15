ed154dd2e0a553326b81b1b1723580ee6b418e98
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-09-15 21:46:33 +0000

    print square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square
+ *
+ * @size: size of square to be printed
+ */
+
+void print_square(int size)
+{
+	int l, i;
+
+	if (size > 0)
+	{
+		for (l = 0; l < size; l++)
+		{
+			for (i = 0; i < size; i++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
