a78b2de2141a158bc88aff729b5c4ef68c3054ad
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-09-15 16:31:15 +0000

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_line - prints line
+ *
+ * @n: range to which to print
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 1; i <= n; i++)
+		{
+			_putchar(_);
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
