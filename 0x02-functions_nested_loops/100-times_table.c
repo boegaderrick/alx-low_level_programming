122cb37ffaccea1f362578eb367e53ae5cfc4b83
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-09-15 01:05:56 +0000

    custom times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,24 +1,18 @@
 #include "main.h"
-#include <math.h>
-
 /**
  * print_times_table - function prints times table
- *
  * @n: input number
  */
-
 void print_times_table(int n)
 {
 	int row, col, prod;
 
 	row = 0;
-
 	if ((n >= 0) && (n <= 15))
 	{
 		while (row <= n)
 		{
 			col = 0;
-
 			while (col <= n)
 			{
 				prod = row * col;
@@ -42,7 +36,7 @@ void print_times_table(int n)
 						_putchar(' ');
 						_putchar((prod / 100) + '0');
 						_putchar((prod / 10 % 10) + '0');
-						_putchar((prod % 10 ) + '0');
+						_putchar((prod % 10) + '0');
 					}
 					else
 					{
