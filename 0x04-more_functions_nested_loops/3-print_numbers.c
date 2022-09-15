fab042e5e07baf42d1a44f2409269257de4ef4c4
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-09-15 13:12:26 +0000

    zero to nine

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0 to 9
+ *
+ */
+
+void print_numbers(void)
+{
+	for (int n = 0; n < 10; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
