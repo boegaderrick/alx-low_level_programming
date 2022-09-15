d81362b7e090c9a99df7dfaff4bdb50ca6dd985a
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-09-15 01:07:43 +0000

    custom times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -39,8 +39,7 @@ void print_times_table(int n)
 						_putchar((prod % 10) + '0');
 					}
 					else
-					{
-						_putchar(',');
+					{	_putchar(',');
 						_putchar(' ');
 						_putchar(' ');
 						_putchar((prod / 10) + '0');
