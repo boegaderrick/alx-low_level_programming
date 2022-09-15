de549a5c2dbef85bc5a36c82ff354a718169135b
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-09-15 12:56:35 +0000

    multiplication func

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multiplie inputs
+ *
+ * @a, @b: inputs
+ *
+ * Return: product of the multiplication
+ */
+
+int mul(int a, int b)
+{
+	int product = a * b;
+	return (product);
+}
