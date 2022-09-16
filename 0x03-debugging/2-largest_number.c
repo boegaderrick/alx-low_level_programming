1e57c9a52c6eab736ad143ebc45f359a5c9d351b
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-09-16 23:58:03 +0000

    largest number check fix

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -8,9 +8,9 @@
  * Return: largest number
  */
 
-int largest_number(int a, int b, int c)
+int largest_number(long a, long b, long c)
 {
-	int largest;
+	long largest;
 
 		if (a > b && a > c)
 		{
@@ -22,7 +22,10 @@ int largest_number(int a, int b, int c)
 		}
 		else
 		{
-			largest = c;
+			if (c > a && c > b)
+			{
+				largest = c;
+			}
 		}
 		return (largest);
 }
