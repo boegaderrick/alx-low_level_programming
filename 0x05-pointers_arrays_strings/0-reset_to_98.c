d50cbbcaf24afcc7325fe387506a48a676accbab
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-09-20 15:00:18 +0000

    first pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* reset_to_98 - updates passed value to 98
+*
+* @n: value to be changed
+*
+* main - entry point
+*
+* Return: 0
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
