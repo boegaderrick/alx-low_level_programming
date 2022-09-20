5f3c735e188504bd930866b5112cea912feced1b
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-09-20 19:05:40 +0000

    strlen strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* _strlen - checks string length
+* @s: pointer to string to be checked
+* Return: returns length of the string
+*/
+
+int _strlen(char *s)
+{
+	int length;
+
+	length = strlen(s);
+	return (length);
+}
