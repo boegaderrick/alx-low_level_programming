4574f2c2c5b3f24e0dafb9385fb9dd86a0a5d91a
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-09-20 21:49:24 +0000

    reverse string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* print_rev - prints strings in reverse
+* @s: pointer to string to be printed
+*/
+
+void print_rev(char *s);
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++
+	}
+	while (len >= 0)
+	{
+		_putchar(s[len]);
+		len--;
+	}
+	_putchar('\n');
+}
