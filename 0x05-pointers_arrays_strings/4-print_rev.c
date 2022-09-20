1c2d8386f42a1cccac30071b1932deafd2b271c5
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-09-20 22:13:56 +0000

    reverse_string

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,13 +7,16 @@
 
 void print_rev(char *s)
 {
-	int len = 0;
+	int len, length;
+	len = 0;
 
 	while (s[len] != '\0')
 	{
 		len++;
 	}
-	int length = len--;
+
+	length = len--;
+
 	while (length >= 0)
 	{
 		_putchar(s[length]);
