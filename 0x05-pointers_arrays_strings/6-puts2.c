7e88aaf988cd6d9d7a91b8f4cb2040b233c610b0
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-09-20 23:50:50 +0000

    puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+* puts2 - prints every other instance
+* @s: parameter to be printed
+*/
+
+void puts2(char *str)
+{
+	int len, length;
+
+	len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	length = len - 1;
+	len = 0;
+
+	while (len <= length)
+	{
+		_putchar(s[len]);
+		len++;
+		len++;
+	}
+	_putchar('\n');
+}
