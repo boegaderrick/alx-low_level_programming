e01e3f53fa86e51336061e5a4cf2b7e22b2b9ac3
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-09-21 00:39:35 +0000

    second half string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+* puts_half - prints second half of string
+* @str: pointer to string to be printed
+*/
+
+void puts_half(char *str)
+{
+	int len, length, half, secondhalf;
+
+	len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	length = len;
+	half = length / 2;
+
+	if (length % 2 != 0)
+	{
+		secondhalf = half + 1;
+	}
+	else
+	{
+		secondhalf = half;
+	}
+	while (secondhalf < length)
+	{
+		_putchar(str[secondhalf];
+				secondhalf++;
+	}
+}
