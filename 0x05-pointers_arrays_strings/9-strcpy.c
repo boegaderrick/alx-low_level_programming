1ccff1b4e8b4d74a9b39f05d016af6d958ac1515
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-09-21 15:42:18 +0000

    strcpy func

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* _strcpy - copies strings
+* @dest: string to be augmented
+* @src: string to be copied
+* Return: dest string
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+	return (dest);
+}
