27447f3f1b7cd40d8af8a996675171500dc8f4ff
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-09-22 18:22:24 +0000

    concatenation to set limit

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+* _strncat - concatenates two strings to a provided limit
+* @dest: string 1
+* @src: string 2
+* @n: limit to concatenate
+* Return: full string/dest
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int len1, len2;
+
+	len1 = 0;
+	len2 = 0;
+
+	while (dest[len1] != '\0')
+		len1++;
+	while (src[len2] != src[n])
+	{
+		dest[len1] = src[len2];
+		len1++;
+		len2++;
+	}
+	if (src[n] > '\0')
+	{
+		dest[len1] = '\0';
+	}
+	else
+		;
+	return (dest);
+}
