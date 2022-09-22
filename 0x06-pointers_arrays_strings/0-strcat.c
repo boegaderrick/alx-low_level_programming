465d241f3d78084a444418744390404fd4b95168
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-09-22 15:43:15 +0000

    concatenation

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,6 +10,9 @@ char *_strcat(char *dest, char *src)
 {
 	int len1, len2;
 
+	len1 = 0;
+	len2 = 0;
+
 	while (dest[len1] != '\0')
 		len1++;
 	while (src[len2] != '\0')
@@ -18,5 +21,6 @@ char *_strcat(char *dest, char *src)
 		len1++;
 		len2++;
 	}
+	dest[len1] = '\0';
 	return (dest);
 }
