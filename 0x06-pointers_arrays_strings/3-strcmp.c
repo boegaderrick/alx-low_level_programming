8aabbc8ff08595f259de380cf419b3b87dbc48d3
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-09-23 02:49:23 +0000

    strcmp func

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* _strcmp - compares size of two strings
+* _strcmp - compares two strings
 * @s1: string 1
 * @s2: string 2
 * Return: 0 if same, positive if str1>  or negative if str2>
@@ -9,29 +9,13 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int len1, len2;
+	int len;
 
-	len1 = 0;
-	len2 = 0;
-
-	while (s1[len1] != '\0')
+	for (len = 0; s1[len] != '\0' || s2[len] != '\0'; len++)
 	{
-		len1++;
+		if (s1[len] != s2[len])
+			return (s1[len] - s2[len]);
 	}
-	while (s2[len2] != '\0')
-	{
-		len2++;
-	}
-	if (len1 > len2)
-	{
-		return (-1);
-	}
-	else if (len1 == len2)
-	{
-		return (0);
-	}
-	else
-	{
-		return (1);
-	}
+	return (0);
 }
+
