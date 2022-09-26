4c2b835b8ef9bbb96ff417c770a9ee4fd1c49dfd
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-09-26 23:13:41 +0000

    strchr func

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* _strchr - looks for character in string
+* @s: subject string
+* @c: subject char
+* Return: pointer s if char present or NULL if not
+*/
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+}
