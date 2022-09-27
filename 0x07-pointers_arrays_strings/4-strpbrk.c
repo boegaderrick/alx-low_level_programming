1874d7fd74f9d79c87e635743d7005e582777073
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-09-27 02:17:16 +0000

    strpbrk func

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* _strpbrk - searches string for any set of bytes
+* @s: string to be searched
+* @accept: chars to be searched for
+* Return: pointer to leading byte if present or NULL if absent
+*/
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (accept[j] == s[i])
+			{
+				char *ptr = &s[i];
+
+				return (ptr);
+			}
+		}
+	}
+	return (s + 1);
+}
