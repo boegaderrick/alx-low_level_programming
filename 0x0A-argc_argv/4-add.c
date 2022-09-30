bd2fc7b970dc88243fd108d2a0857e1c276a7605
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-09-30 16:03:39 +0000

    arg sum

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,7 +10,7 @@
 
 int main(int argc, char *argv[])
 {
-	int i, sum;
+	int i, j, sum;
 
 	sum = 0;
 
@@ -18,15 +18,15 @@ int main(int argc, char *argv[])
 	{
 		for (i = 1; i < argc; i++)
 		{
-			if (*argv[i] >= '0' && *argv[i] <= '9')
+			for (j = 0; argv[i][j] != '\0'; j++)
 			{
-				sum = sum + atoi(argv[i]);
+				if (argv[i][j] < '0' || argv[i][j] > '9')
+				{
+					printf("Error\n");
+					return (1);
+				}
 			}
-			else
-			{
-				printf("Error\n");
-				return (1);
-			}
+			sum = sum + atoi(argv[i]);
 		}
 		printf("%d\n", sum);
 		return (0);
