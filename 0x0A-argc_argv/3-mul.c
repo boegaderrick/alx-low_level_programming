3992db553b9c946bba05273fcd7c65d7d8bf87be
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-09-30 13:11:07 +0000

    arg mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - finds the product of argvs
+* @argc: argument count
+* @argv: argument array
+* Return: 0 if successful or 1 if operation failed
+*/
+
+int main(int argc, char *argv[])
+{
+	int prod, i;
+
+	prod = 1;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			prod = prod * atoi(argv[i]);
+		}
+		printf("%d\n", prod);
+		return (0);
+	}
+	printf("Error\n");
+	return (1);
+}
