b927de379e1dd1478fc90b72fe4b19884f454bc1
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-09-30 11:04:27 +0000

    argc - count print

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - program start
+* @argc: number of arguments
+* @argv: array of arguments
+* Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+		printf("%d\n", argc - 1);
+		return (0);
+}
