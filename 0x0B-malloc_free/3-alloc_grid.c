824a4f81db83f891792f0c4073e5e9e9b0f21843
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-10-04 18:53:36 +0000

    2d malloc

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -27,10 +27,6 @@ int **alloc_grid(int width, int height)
 	{
 		array[i] = malloc(sizeof(int) * width);
 	}
-	if (array[i] == NULL)
-	{
-		return (NULL);
-	}
 	for (i = 0; i < height; i++)
 	{
 		for (j = 0; j < width; j++)
