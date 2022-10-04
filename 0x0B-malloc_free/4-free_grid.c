09f6f90ff3b188d31d5d0a2c4f9503c9dd12228e
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-10-04 19:25:55 +0000

    freealloc

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* free_grid - frees dynamically allocated memory
+* @grid: pointer to pointer to memory area
+* @height: dimension of allocated memory
+*/
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+	return;
+}
