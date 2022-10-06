8028b76b2cb8b42b46e5930c540c984b178d2974
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-10-06 13:56:21 +0000

    malloc concat to limit

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,55 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* length - calculates length of string s1
+* @s1: subject string
+* Return: length
+*/
+
+unsigned int length(char *s1)
+{
+	unsigned int i, len = 0;
+
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		len++;
+	}
+	len++;
+	return (len);
+}
+
+/**
+* string_nconcat - concatenates strings to specified length limit
+* @s1: subject string 1
+* @s2: subject string 2
+* @n: length limit for string 2
+* Return: final string on NULL if errors encoutered
+*/
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i, j, len;
+	char *array;
+
+	len = length(s1);
+	array = malloc(sizeof(char) * (len + n));
+
+	if (array == NULL)
+	{
+		return (NUL);
+	}
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		array[i] = s1[i];
+	}
+	for (j = 0; j < n; j++)
+	{
+		array[i] = s2[j];
+		i++;
+	}
+	array[i] = '\0';
+
+	return (array);
+}
