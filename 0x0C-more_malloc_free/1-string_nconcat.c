232e44c2ed6f39ed7dba06ed89e0aaed10d1db1e
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-10-06 14:34:52 +0000

    malloc concat to limit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,9 +12,12 @@ unsigned int length(char *s1)
 {
 	unsigned int i, len = 0;
 
-	for (i = 0; s1[i] != '\0'; i++)
+	if (s1 != NULL)
 	{
-		len++;
+		for (i = 0; s1[i] != '\0'; i++)
+		{
+			len++;
+		}
 	}
 	len++;
 	return (len);
@@ -40,14 +43,20 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		return (NULL);
 	}
-	for (i = 0; s1[i] != '\0'; i++)
+	if (s1 != NULL)
 	{
-		array[i] = s1[i];
+		for (i = 0; s1[i] != '\0'; i++)
+		{
+			array[i] = s1[i];
+		}
 	}
-	for (j = 0; j < n; j++)
+	if (s2 != NULL)
 	{
-		array[i] = s2[j];
-		i++;
+		for (j = 0; j < n; j++)
+		{
+			array[i] = s2[j];
+			i++;
+		}
 	}
 	array[i] = '\0';
 
