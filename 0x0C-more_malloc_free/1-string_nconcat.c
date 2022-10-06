a6291910ac2f73a80e46bc4e54eac643f69b0ed7
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-10-06 14:45:38 +0000

    malloc concat to limit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -33,7 +33,7 @@ unsigned int length(char *s1)
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, j, len;
+	unsigned int i = 0, j, len;
 	char *array;
 
 	if (s1 == NULL && s2 == NULL)
