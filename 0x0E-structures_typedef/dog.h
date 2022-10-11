bc179b6c89b6a56f68aba10f61546cb1dd4b86e8
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-10-11 22:07:18 +0000

    struct header

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+* struct dog - stores details about dog
+* @name: stores dog's name
+* @age: stores dog's age
+* @owner: stores owner's name
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
