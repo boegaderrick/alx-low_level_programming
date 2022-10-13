9871676738eb091953bbc22d538dad7091a99264
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-10-13 17:26:37 +0000

    array iteration

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,16 @@
+/**
+* array_iterator - executes function for each element of array
+* @array: subject array
+* @size: size of array
+* @action: pointer to function to be executed
+*/
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
