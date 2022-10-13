4905f898c53e34a839e76c124387d6ae2ac35cb5
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-10-13 16:45:15 +0000

    function pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,11 @@
 #include "function_pointers.h"
 
+/**
+* print_name - calls function
+* @name: argument to be passed to called function
+* @f: pointer to function to be called
+*/
+
 void print_name(char *name, void (*f)(char *));
 {
 	f(name);
