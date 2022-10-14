00868f47b38d70c1540d33e91cb624ba7877b367
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-10-14 21:47:55 +0000

    print nums

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -21,10 +21,13 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		for (i = 0; i < n; i++)
 		{
 			num = va_arg(param, int);
-			printf("%d", num);
-			if (separator != NULL && i <= (n - 2))
+			if (num != NULL)
 			{
-				printf("%s", separator);
+				printf("%d", num);
+				if (separator != NULL && i <= (n - 2))
+				{
+					printf("%s", separator);
+				}
 			}
 		}
 	}
