61b2967eccdf1c722ab852604831738f0bc2d88a
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-10-14 21:10:57 +0000

    print nums

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -10,13 +10,14 @@
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	unsigned int i;
+	const unsigned int i;
 	int num;
 	va_list param;
 
+	va_start(param, n);
 	if (n > 0)
 	{
-		va_start(param, n);
+		/*va_start(param, n);*/
 
 		for (i = 0; i < n; i++)
 		{
