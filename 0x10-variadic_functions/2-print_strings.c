1f1425edae56f71d0ced2cc9c557f9680f41c5fc
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-10-14 21:01:48 +0000

    print strings

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,36 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+* print_strings - prints strings
+* @separator: parameter to separate strings
+* @n: number of strings
+*/
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	char *s;
+	va_list strings;
+
+	va_start(strings, n);
+	for (i = 0; i < n; i++)
+	{
+		s = va_arg(strings, char *);
+		if (s == NULL)
+		{
+			printf("(nil)");
+		}
+		else
+		{
+			printf("%s", s);
+		}
+		if (separator != NULL && i <= (n - 2))
+		{
+			printf("%s", separator);
+		}
+	}
+	va_end(strings);
+	printf("\n");
+}
