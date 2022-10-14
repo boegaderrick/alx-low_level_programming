1ff8f77d85edc40fa993d9d556ce0fe535dfd6bf
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-10-14 19:29:05 +0000

    print nums - variadic funcs

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,30 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+* print_numbers - prints numbers
+* @separator: pointer to string that separates each number
+* @n: count of numbers to be printed
+*/
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	int num;
+	va_list param;
+
+	va_start(param, n);
+
+	for (i = 0; i < n; i++)
+	{
+		num = va_arg(param, int);
+		printf("%d", num);
+		if (separator != NULL && i <= (n - 2))
+		{
+			printf("%s", separator);
+		}
+	}
+	va_end(param);
+	printf("\n");
+}
