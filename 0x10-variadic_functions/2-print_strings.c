26d8d59ebe3eb613ac43581e3fc9020b7d98cd5d
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-10-15 00:07:03 +0000

    print strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -28,7 +28,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			{
 				printf("%s", s);
 			}
-			if (separator != NULL && i <= (n - 2))
+			if (separator != NULL && i <= (n - 2) && i > 1)
 			{
 				printf("%s", separator);
 			}
