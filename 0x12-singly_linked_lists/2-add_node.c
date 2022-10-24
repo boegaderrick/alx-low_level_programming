bb001d4d713a1c24b9380f48fa42ececcedeb289
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-10-24 20:22:15 +0000

    unction adds node to end of list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,6 +1,23 @@
 #include "lists.h"
 
 /**
+* _strlen - gets length of a string
+* @string: subject string
+* Return: length
+*/
+
+int _strlen(const char *string)
+{
+	int i;
+
+	for (i = 0; string[i] != '\0'; i++)
+	{
+		continue;
+	}
+	return (i);
+}
+
+/**
 * add_node - adds node to beginning of a linked list
 * @head: address of pointer to linked list
 * @str: element to be included in new node
