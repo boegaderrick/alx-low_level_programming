b3b1a051b3edb7d0e7e9a7069e2ac167ecd879ea
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-10-26 06:58:58 +0000

    more singly linked lists

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+* print_listint - prints all elements of a linked list
+* @h: pointer to address of first node of linked list
+* Return: number of nodes
+*/
+
+size_t print_listint(const listint_t *h)
+{
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
