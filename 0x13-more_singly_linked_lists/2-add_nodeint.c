9d7a667b5043441501cac060c452c8953977e8f6
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-10-26 07:30:36 +0000

    add new node at start of linked list

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+* add_nodeint - adds node at the beginning of linked list
+* @head: pointer to pointer of address of first node
+* @n: number to be held in new node
+* Return: pointer to new node at start of list, NULL if operation failed
+*/
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *ptr;
+
+	ptr = malloc(sizeof(listint_t));
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	ptr->n = n;
+	ptr->next = *head;
+	*head = ptr;
+	return (*head);
+}
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -3,7 +3,7 @@
 
 /* HEADER FILES */
 #include <stdio.h>
-
+#include <stdlib.h>
 
 /**
  * struct listint_s - singly linked list
@@ -19,9 +19,9 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
-
 /* FUNCTION PROTOTYPES */
 size_t print_listint(const listint_t *);
 size_t listint_len(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
 
 #endif
