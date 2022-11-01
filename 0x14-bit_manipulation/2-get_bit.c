67ab9a740b211824906a33bd7ce09927c27ab9d1
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-11-01 14:57:22 +0000

    get value at index

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* get_bit - gets value of a bit at given index
+* @n: subject binary number
+* @index: index at which bit is located
+* Return: value of bit
+*/
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int value = n >> index & 1 ? 1 : 0;
+
+	return (value);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -10,5 +10,6 @@ unsigned int binary_to_uint(const char *);
 int _strlen(const char *b);
 int _pow(int num, int power);
 void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
 
 #endif /* MAIN_H */
