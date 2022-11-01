8f235aa39c49fd9c134739f045bf963a2ab08ca5
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-11-01 08:57:39 +0000

    binary to int conversion

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,65 @@
+#include "main.h"
+
+/**
+* _strlen - gets lenght of a string
+* @b: subject string
+* Return: length of string
+*/
+int _strlen(const char *b)
+{
+	int len;
+
+	for (len = 0; b[len] != '\0'; len++)
+	{
+		;
+	}
+	return (len);
+}
+
+/**
+* _pow - multiplies number to a given power
+* @num: number to be multiplied
+* @power: power extent
+* Return: result of operation
+*/
+int _pow(int num, int power)
+{
+	int res = num;
+
+	if (power == 0)
+		return (1);
+	if (power == 1)
+		return (num);
+	while (power > 1)
+	{
+		res *= num;
+		power--;
+	}
+	return (res);
+}
+
+/**
+* binary_to_uint - converts binary number to decimal
+* @b: string containing binary number to be converted
+* Return: decimal number
+*/
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int num = 0, i, j;
+
+	if (b == NULL)
+		return (0);
+
+	j = _strlen(b) - 1;
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+		if (b[i] != '0')
+		{
+			num += _pow(2, j);
+		}
+		j--;
+	}
+	return (num);
+}
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* HEADER FILES */
+#include <stddef.h>
+
+/* FUNCTION PROTOTYPES */
+unsigned int binary_to_uint(const char *);
+int _strlen(const char *b);
+int _pow(int num, int power);
+
+#endif /* MAIN_H */
