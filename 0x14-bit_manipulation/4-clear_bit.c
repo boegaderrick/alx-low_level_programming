520178ba9a001ff288de3580b80512db3485a9c2
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-11-01 16:36:58 +0000

    unset bit

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* clear_bit - unsets bit at given index
+* @n: pointer to address of number to be altered
+* @index: index of bit to be unset
+* Return: 1 or -1 if error/s occured
+*/
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (n == NULL)
+		return (-1);
+	*n &= ~(1 << index);
+	return (1);
+}
