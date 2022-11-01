374dc072ea8b692cad0e1ae0d53a9183186d6ee8
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-11-01 15:59:22 +0000

    set bit at index

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* set_bit - sets bit at given index to 1
+* @n: pointer to address of int to be modified
+* @index: index of bit to be set
+* Return: 1 or -1 if error occured
+*/
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (n == NULL)
+		return (-1);
+	*n |= (1 << index);
+	return (1);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -11,5 +11,6 @@ int _strlen(const char *b);
 int _pow(int num, int power);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
 
 #endif /* MAIN_H */
