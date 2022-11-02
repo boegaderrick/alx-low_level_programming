f6c0b97e07b9e61e6036e7c006481f0e0e286176
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-11-02 18:28:14 +0000

    file I/O read and write

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+* read_textfile - reads a file and prints contents to stdout
+* @filename: file to be read
+* @letters: number of characters to be read printed
+* Return: total chars printed or 0 if error/s occured
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, write_fail;
+	char *buffer;
+	ssize_t count;
+
+	if (filename == NULL || letters < 1)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+
+	count = read(fd, buffer, letters);
+	write_fail = write(1, buffer, letters);
+	if (!write_fail)
+		return (0);
+
+	close(fd);
+	free(buffer);
+	return (count);
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* HEADER FILES */
+#include <stddef.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+/* PROTOTYPES */
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif /* MAIN_H */
