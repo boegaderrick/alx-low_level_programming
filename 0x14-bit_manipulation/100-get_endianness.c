96a3564c3c989793363f3d7642193b790b09cd87
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-11-23 11:11:42 +0000

    endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* get_endianness - checks if cpu is little or big endian
+* Return: 1 if little, 0 if big
+*/
+int get_endianness(void)
+{
+	int num = 1;
+	char *ptr = (char *)&num;
+
+	if (ptr == 1)
+		return (1);
+	else
+		return (0);
+}
