7be507a6582934b3fa17945c853a70bc7ed07ec1
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-12-12 13:02:14 +0000

    insert node at index in doubly linked list

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -40,6 +40,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		ptr = ptr->next;
 		i++;
 	}
-	new = add_dnodeint_end(h, n);
+	/*new = add_dnodeint_end(h, n);*/
 	return (new);
 }
