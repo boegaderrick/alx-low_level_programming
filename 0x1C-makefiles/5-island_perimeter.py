b1d5f52458ce43296153f081bbbf51fed2ffaf57
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-02-24 14:16:39 +0300

    technical interview prep

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -9,26 +9,16 @@ def island_perimeter(grid):
     One represents land area and zero represents water area
     Every valid cell (ones) is a square of side length 1
     """
-    """if type(grid) is not list:
+    if type(grid) is not list:
         return 0
     if len(grid) < 2:
         return 0
     if not all(type(i) is list for i in grid):
-        return 0"""
+        return 0
 
     length = 0
     width = 1
     perimeter = 0
-
-    if not all(type(i) is list for i in grid):
-        for i in grid:
-            if type(i) is not int:
-                return 0
-            if i > 0:
-                length += 1
-        if length > 0:
-            perimeter = 2 * (lenth + width)
-        return perimeter
 
     for i in grid:
         for j in i:
