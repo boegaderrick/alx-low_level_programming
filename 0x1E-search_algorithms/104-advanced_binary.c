19261b62aadd71e2636d50a25a94e1caab696ed8
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-04-13 23:17:40 +0300

    advanced binary algorithm

---
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -0,0 +1,61 @@
+#include "search_algos.h"
+
+/**
+ * helper - recursive function to aid in implemetation of the algorithm
+ * @array: array to perform search on
+ * @value: value to search for
+ * @size: number of items in array
+ * @idx: pointer to store index if found
+ * @low: lower bound of search index range
+ * @high: upper bound
+ */
+void helper(int *array, int value, int size, int *idx, int low, int high)
+{
+	int mid, i;
+
+	/*if (*idx != -1)*/
+	/*	return;*/
+	if (high >= low)
+	{
+		printf("Searching in array: ");
+		for (i = low; i <= high; i++)
+		{
+			printf("%d", array[i]);
+			if (i < high)
+				printf(", ");
+			else
+				printf("\n");
+		}
+		mid = low + ((high - low) / 2);
+		if (array[mid] == value)
+		{
+			while (mid > 0 && array[mid - 1] == value)
+				mid--;
+			*idx = mid;
+			return;
+		}
+		if (array[mid] > value)
+			helper(array, value, size, idx, low, mid - 1);
+		else
+			helper(array, value, size, idx, mid + 1, high);
+	}
+}
+
+/**
+ * advanced_binary - performs a search using binary search algorithm
+ * @array: array to perform search on
+ * @size: number of items in array
+ * @value: value to search for
+ *
+ * Return: index of value if found, -1 otherwise
+ */
+int advanced_binary(int *array, size_t size, int value)
+{
+	int idx = -1;
+
+	if (array == NULL)
+		return (idx);
+
+	helper(array, value, size, &idx, 0, size - 1);
+	return (idx);
+}
